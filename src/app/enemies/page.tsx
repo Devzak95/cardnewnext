578ff035519e08a7ee224f1ba91e65a6cd8e39d7
devzak95 <devzak95@hotmail.com>
2025-01-08 10:54:39 +0100

    finish card

--- src/app/enemies/page.tsx
+++ src/app/enemies/page.tsx
@@ -1,13 +1,77 @@
+"use client";
+
+import Image from "next/image";
 import card from "../page.module.scss";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { useSearchParams } from "next/navigation";
+import { EnemiesArray } from "@/types/type";
+
+// const data: EnemiesArray = [masser af data]
 
 const EnemiesPage = () => {
+  const data = [
+    {
+      name: "Alien Scum",
+      img: "/assets/black-alien-scum.png",
+      text: "Ninja Killer dont touch him",
+    },
+
+    {
+      name: "Death Smokie",
+      img: "/assets/death-smokie.png",
+      text: "Black Scum is a Ninja Killer",
+    },
+
+    {
+      name: "Ping Pang",
+      img: "/assets/angry-ping.png",
+      text: "Just som penguin Shit",
+    },
+  ];
+
+  const searchParams = useSearchParams();
+  const query = searchParams.get("id");
+
+  const dataID = query as string;
+  const id = parseInt(dataID);
+  // console.log(id);
+  //! man kan også gøre på den her måde
+  /*
+  const searchParams = useSearchParams();
+  const query = searchParams.get("id");
+  const id = Number(query)
+  */
+
+  const refObj = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    gsap.to(refObj.current, {
+      duration: 0.05,
+      scaleX: -1,
+      alpha: 0,
+      repeat: 3,
+      yoyo: true,
+      onComplete: () => {
+        gsap.to(refObj.current, {
+          duration: 0.05,
+          alpha: 1,
+          scaleX: 1,
+        });
+      },
+    });
+  }, [id]);
+
   return (
     <main>
-      <section id={card.cardContainer}>
+      <section ref={refObj} id={card.cardContainer}>
         <div id={card.card}>
-          <div id={card.headline}>Alien Scum</div>
-          <div id={card.imagecon}></div>
-          <div id={card.content}>Ninja Killer don't touch him</div>
+          <div id={card.headline}>{data[id].name}</div>
+          <div id={card.imagecon}>
+            {/* på Image tag kan man pu fill for at billedet fylder den container den ligger i */}
+            <Image src={data[id].img} alt="picture" width={250} height={250} />
+          </div>
+          <div id={card.content}>{data[id].text}</div>
         </div>
       </section>
     </main>
